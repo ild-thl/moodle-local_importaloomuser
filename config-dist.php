0fb77bbc7bf29dec2c490e841fe9247d079e544d
ISy THL <dev.ild@th-luebeck.de>
2023-06-08 14:18:47 +0200

    Added config-dist.php as config example

---
+++ config-dist.php
@@ -0,0 +1,11 @@
+<?php  // Plugin configuration file
+/**
+ * Link to CSV user upload
+ *
+ * @package    local
+ * @subpackage importaloomuser
+ * @copyright   2023 ILD TH Lübeck <dev.ild@th-luebeck.de>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+$token = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+$event_id = "00000";
